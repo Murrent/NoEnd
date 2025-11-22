6a899554caf118ee494d07f983cf41cb61ceaf29
Murrent <43298787+Murrent@users.noreply.github.com>
2025-11-22 23:26:12 +0100

    Arrows now stick to things

---
+++ NoEnd/Assets/Scripts/Weapons/Arrow.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+public class Arrow : MonoBehaviour
+{
+    [SerializeField] private LayerMask _layerMask;
+    [SerializeField] private Transform _tip;
+    private Rigidbody _rb;
+    private Vector3 _prevPosition;
+
+    void Start()
+    {
+        _prevPosition = _tip.position;
+        _rb = GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate()
+    {
+        if (Physics.Linecast(_prevPosition, _tip.position, out var hit, _layerMask))
+        {
+            transform.position = hit.point - _tip.TransformVector(_tip.localPosition);
+            _rb.constraints = RigidbodyConstraints.FreezeAll;
+            transform.SetParent(hit.transform);
+            Destroy(_rb);
+            Destroy(this);
+            return;
+        }
+
+        _prevPosition = _tip.position;
+    }
+}
