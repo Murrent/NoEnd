6f0a29c856a647609d7e935d0ed64f1560268301
XXXFIRESPIN123GODXXX <eson000526@gmail.com>
2025-11-22 14:40:15 +0100

    Bomb script
    - Added bomb script
    - variable fuse timer and radius
    - added Audio folder
    - added temp bomb audio

---
+++ NoEnd/Assets/Scripts/Player.cs
@@ -0,0 +1,119 @@
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField]
+    Camera _camera;
+
+    [SerializeField]
+    InputActionReference _mousePositionReference;
+    InputAction _mousePosition;
+
+    [SerializeField]
+    InputActionReference _mousePressReference;
+    InputAction _mousePress;
+
+    bool _isDragging = false;
+
+    Vector3 _startDragPosition;
+
+    Plane _groundPlane = new Plane(Vector3.down, Vector3.zero);
+
+    private void OnEnable()
+    {
+        if (_mousePress is not null)
+        {
+            _mousePress.performed += OnPress;
+            _mousePress.canceled += OnReleased;
+        }
+    }
+
+    private void OnDisable()
+    {
+        if (_mousePress is not null)
+        {
+            _mousePress.performed -= OnPress;
+            _mousePress.canceled -= OnReleased;
+        }
+    }
+
+    private void Awake()
+    {
+        if(!_camera)
+        {
+            _camera = Camera.main;
+            Debug.LogWarning($"No \"_camera\" is assigned to gameobject \"{gameObject.name}\", assuming main camera", this);
+        }
+
+        if (_mousePositionReference)
+        {
+            _mousePosition = _mousePositionReference.ToInputAction();
+            _mousePosition.Enable();
+        }
+        else
+        {
+            Debug.LogError($"No \"_mousePositionReference\" input action reference is assigned to gameobject \"{gameObject.name}\"", this);
+        }
+
+        if (_mousePressReference)
+        {
+            _mousePress = _mousePressReference.ToInputAction();
+            _mousePress.Enable();
+
+            _mousePress.performed += OnPress;
+        }
+        else
+        {
+            Debug.LogError($"No \"_mousePressReference\" input action reference is assigned to gameobject \"{gameObject.name}\"", this);
+        }
+    }
+
+    private void Update()
+    {
+        if (_isDragging)
+        {
+            Debug.DrawLine(_startDragPosition, GetHandPosition(), Color.green);
+        }
+    }
+
+    void OnPress(InputAction.CallbackContext callbackContext)
+    {
+        _isDragging = true;
+
+        _startDragPosition = GetHandPosition();
+    }
+
+    void OnReleased(InputAction.CallbackContext callbackContext)
+    {
+        _isDragging = false;
+    }
+
+    Vector3 GetHandPosition()
+    {
+        Ray ray = _camera.ScreenPointToRay(_mousePosition.ReadValue<Vector2>());
+
+        if(_groundPlane.Raycast(ray, out float distanceToGroundPlane))
+        {
+            Vector3 handPosition = ray.origin + ray.direction * distanceToGroundPlane - _camera.transform.forward;
+            return handPosition;
+        }
+        else
+        {
+            Debug.LogError("Invalid ray", this);
+            return Vector3.zero;
+        }
+    }
+
+#if UNITY_EDITOR
+    private void OnDrawGizmos()
+    {
+        if (Application.isPlaying)
+        {
+            Gizmos.color = Color.red;
+            Gizmos.DrawSphere(GetHandPosition(), 0.2f);
+        }
+    }
+#endif
+}
