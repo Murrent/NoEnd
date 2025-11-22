50fd94d5c6b282157245cd090e17f87d56bd214a
Murrent <43298787+Murrent@users.noreply.github.com>
2025-11-22 17:08:30 +0100

    Large hammer configured

--- NoEnd/Assets/Scripts/Weapons/Maul.cs
+++ NoEnd/Assets/Scripts/Weapons/Maul.cs
@@ -1,18 +1,16 @@
-using System;
 using UnityEngine;
 
 public class Maul : Weapon
 {
-    private Vector3 _velocity;
-    private Vector3 _moveVector;
-    private Vector3 _diff;
-    [SerializeField] private Transform _end;
+    [SerializeField] private Rigidbody _root;
+    [SerializeField] private Rigidbody _end;
+    [SerializeField] private Transform _texture;
 
     public override void MoveWeapon(Vector3 position)
     {
-        Vector3 diff = position - transform.position;
-        _velocity += new Vector3(diff.x, 0.0f, diff.z);
-        transform.position = position;
+        _root.MovePosition(position);
+        _texture.position = position;
+        _texture.rotation = Quaternion.LookRotation((_end.position - _root.position).normalized, Vector3.up);
     }
 
     public override void Use()
@@ -21,31 +19,5 @@ public class Maul : Weapon
 
     public override void Release()
     {
-    }
-
-    private void FixedUpdate()
-    {
-        _velocity = Vector3.Lerp(_velocity, Vector3.zero, Time.fixedDeltaTime);
-        _diff = (_end.position - transform.position).normalized;
-        Vector2 diff2D = new Vector2(_diff.x, _diff.z);
-        Vector2 perp2D = Vector2.Perpendicular(diff2D);
-        Vector3 perp3D = new Vector3(perp2D.x, 0.0f, perp2D.y);
-        float direction = Vector3.Dot(_velocity, perp3D);
-        //direction = Mathf.Clamp(direction, -1, 1);
-        //direction = direction > 0 ? 1.0f : -1.0f;
-        _moveVector = perp3D * (_velocity.magnitude * direction);
-        _end.position = transform.position + _diff * 5.0f + _moveVector * Time.fixedDeltaTime;
-    }
-
-    private void OnDrawGizmos()
-    {
-        Gizmos.color = Color.red;
-        Gizmos.DrawLine(transform.position, _end.position);
-        Gizmos.color = Color.blue;
-        Gizmos.DrawLine(_end.position, _end.position + _moveVector * 3.0f);
-        Gizmos.color = Color.green;
-        Gizmos.DrawLine(_end.position, _end.position + _velocity * 3.0f);
-        Gizmos.color = Color.black;
-        Gizmos.DrawLine(_end.position, _end.position + _velocity * 3.0f);
     }
 }
