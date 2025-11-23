1927dcc963a3ab22bace21f19ed1ad910e9f7974
Vilgot <122538342+VilgotBohlenius@users.noreply.github.com>
2025-11-23 16:38:05 +0100

    added level sequence

---
+++ NoEnd/Assets/NPCMovement.cs
@@ -0,0 +1,97 @@
+using TMPro;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class NPCMovement : MonoBehaviour
+{
+    Rigidbody _rigidbody;
+
+    [SerializeField]
+    Transform _animationPivot;
+
+    [SerializeField]
+    AnimationCurve _accelerationOverSpeed;
+
+    [SerializeField]
+    float _acceleration;
+
+    [SerializeField, Range(0.0f, 1.0f)]
+    float _deceleration;
+
+    [SerializeField]
+    float _maxSpeed;
+
+    [SerializeField]
+    float _minTargetDistance;
+
+    Vector3 _targetPosition;
+
+    private void Awake()
+    {
+        _rigidbody = GetComponent<Rigidbody>();
+
+        _targetPosition = transform.position;
+    }
+
+    private void FixedUpdate()
+    {
+        if (_animationPivot && _rigidbody.linearVelocity.magnitude > 0.2f)
+        {
+            _animationPivot.forward = _rigidbody.linearVelocity.normalized;
+        }
+
+        _rigidbody.linearVelocity *= 1.0f - _deceleration;
+
+        bool isWithinMinTargetDistance = IsWithinMinTargetDistance();
+        if (!isWithinMinTargetDistance)
+        {
+            // Accelerate
+            _rigidbody.AddForce(GetTargetDirection() * _acceleration * _accelerationOverSpeed.Evaluate(GetCurrentSpeed() / _maxSpeed), ForceMode.Acceleration);      
+        }
+    }
+
+    Vector3 GetTargetDirection()
+    {
+        Vector3 direction = (_targetPosition - transform.position).normalized;
+        return direction;
+    }
+
+    float GetCurrentSpeed()
+    {
+        float speed = _rigidbody.linearVelocity.magnitude;
+        return speed;
+    }
+
+    public void SetTargetPosition(Vector3 targetPosition)
+    {
+        _targetPosition = targetPosition;
+    }
+
+    public bool IsWithinMinTargetDistance()
+    {
+        Vector3 from = transform.position;
+        from.y = 0.0f;
+        Vector3 to = _targetPosition;
+        to.y = 0.0f;
+
+        return Vector3.Distance(from, to) <= _minTargetDistance;
+    }
+
+    public void SetTargetMinDistance(float minTargetDistance)
+    {
+        _minTargetDistance = minTargetDistance;
+    }
+
+    public float GetTargetMinDistance()
+    {
+        return _minTargetDistance;
+    }
+
+#if UNITY_EDITOR
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.blue;
+        Gizmos.DrawRay(transform.position, GetTargetDirection() * 3.0f);
+    }
+#endif
+}
---
+++ NoEnd/Assets/Scripts/King.cs
@@ -0,0 +1,117 @@
+using System.Collections;
+using UnityEngine;
+
+public class King : MonoBehaviour
+{
+    [SerializeField]
+    GameObject _meshParent;
+
+    [SerializeField]
+    NPCMovement _npcMovement;
+
+    [SerializeField]
+    Transform _carriage;
+
+    [SerializeField]
+    float _carriageTravelDistance;
+
+    [SerializeField]
+    float _carriageTravelDuration;
+
+    [SerializeField]
+    Transform[] _flowers;
+
+    [SerializeField]
+    float _smellDuration;
+
+    Vector3 _carriageRestPosition;
+
+    private void Awake()
+    {
+        _carriageRestPosition = _carriage.position;
+
+        _npcMovement.enabled = false;
+        _meshParent.SetActive(false);
+
+        StartCoroutine(DoSequence());
+    }
+
+    IEnumerator DoSequence()
+    {
+        yield return CarriageEntersScreen_Coroutine();
+        _npcMovement.enabled = true;
+        _meshParent.SetActive(true);
+        yield return SmellFlowers_Coroutine();
+        yield return GoTo_Coroutine(_carriage.position);
+        _npcMovement.enabled = false;
+        _meshParent.SetActive(false);
+        yield return CarriageExitsScreen_Coroutine();
+    }
+
+    IEnumerator SmellFlowers_Coroutine()
+    {
+        for (int i = 0; i < _flowers.Length; ++i)
+        {
+            yield return GoTo_Coroutine(_flowers[i].position);
+            yield return new WaitForSeconds(_smellDuration);
+        }
+    }
+
+    IEnumerator CarriageEntersScreen_Coroutine()
+    {
+        Vector3 carriageStartPosition = _carriage.position + Vector3.back * _carriageTravelDistance;
+        for (float t = 0.0f; t < 1.0f; t += Time.deltaTime / _carriageTravelDuration)
+        {
+            _carriage.position = Vector3.Lerp(carriageStartPosition, _carriageRestPosition, 1.0f - Mathf.Pow(1.0f - t, 4.0f));
+
+            yield return new WaitForEndOfFrame();
+        }
+
+        _carriage.position = _carriageRestPosition;
+    }
+
+    IEnumerator CarriageExitsScreen_Coroutine()
+    {
+        Vector3 carriageEndPosition = _carriage.position + Vector3.forward * _carriageTravelDistance;
+        for (float t = 0.0f; t < 1.0f; t += Time.deltaTime / _carriageTravelDuration)
+        {
+            _carriage.position = Vector3.Lerp(_carriageRestPosition, carriageEndPosition, Mathf.Pow(t, 4.0f));
+
+            yield return new WaitForEndOfFrame();
+        }
+
+        _carriage.position = carriageEndPosition;
+    }
+
+    IEnumerator GoTo_Coroutine(Vector3 position)
+    {
+        _npcMovement.SetTargetPosition(position);
+        yield return new WaitUntil(() => { return _npcMovement.IsWithinMinTargetDistance(); });
+    }
+
+#if UNITY_EDITOR
+    private void OnDrawGizmos()
+    {
+        if (_carriage)
+        {
+            Gizmos.color = Color.red;
+            Gizmos.DrawRay(_carriage.position, Vector3.forward * _carriageTravelDistance);
+            Gizmos.DrawRay(_carriage.position, Vector3.back * _carriageTravelDistance);
+            Gizmos.DrawSphere(_carriage.position + Vector3.forward * _carriageTravelDistance, 0.5f);
+            Gizmos.DrawSphere(_carriage.position + Vector3.back * _carriageTravelDistance, 0.5f);
+        }
+
+        Gizmos.color = Color.magenta;
+
+        for (int i = 0; i < _flowers.Length - 1; ++i)
+        {    
+            Gizmos.DrawLine(_flowers[i].position, _flowers[i + 1].position);   
+        }
+
+        for (int i = 0; i < _flowers.Length; ++i)
+        {
+            Gizmos.DrawWireSphere(_flowers[i].position, _npcMovement.GetTargetMinDistance());
+        }
+    }
+#endif
+}
